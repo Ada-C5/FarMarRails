722c7865bed20d436c52a81d247ab2992a30914e
justine <justine.winnie@gmail.com>
2016-04-26 16:04:12 -0700

    created relationships between models.

--- app/models/product.rb
+++ app/models/product.rb
@@ -1,2 +1,4 @@
 class Product < ActiveRecord::Base
+  belongs_to :vendor
+  belongs_to :product_sale
 end
--- app/models/product_sale.rb
+++ app/models/product_sale.rb
@@ -1,2 +1,4 @@
 class ProductSale < ActiveRecord::Base
+  has_many :sales
+  has_many :products
 end
