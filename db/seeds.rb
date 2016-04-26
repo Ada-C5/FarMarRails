8585341b5f4eb32c06ba69318dd1c478abc5da57
Deirdre Storck <deirdre.storck@gmail.com>
2016-04-26 16:38:17 -0700

    justine is a genius and came up with a sweet way to seed the database using an each loop . git add .git add .git add . yeah

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,32 @@
 #
 #   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
 #   Mayor.create(name: 'Emanuel', city: cities.first)
+
+require 'csv'
+# FILE_PATH = "db/seed_csvs/#{}.csv"
+#
+# files = {}
+#
+# CSV.foreach(FILE_PATH, headers: true) do |row|
+#   row_hash = row.to_h
+#   row_hash.delete("fake")
+#   Market.create(row_hash)
+# end
+
+
+hash = {
+ market: 'seed_csvs/markets.csv',
+ product_sale: 'seed_csvs/product_sales.csv',
+ product: 'seed_csvs/products.csv',
+ sale: 'seed_csvs/sales.csv',
+ vendor: 'seed_csvs/vendors.csv',
+}
+
+hash.each do |key, value|
+ file_path = value
+ CSV.foreach(file_path, headers: true) do |row|
+   row_hash = row.to_h
+   row_hash.delete("fake")
+   key.to_s.camelize.constantize.create(row_hash)
+ end
+end
