03f070d98d5c45631c1bc0b93d17ff271b28abd9
Jessica <jessica.weeber@gmail.com>
2016-04-27 15:10:27 -0700

    completed update, edit, create for markets

--- app/controllers/markets_controller.rb
+++ app/controllers/markets_controller.rb
@@ -7,7 +7,36 @@ class MarketsController < ApplicationController
 	def show
 		@markets = Market.where(id: params[:id])
 		@vendors = Vendor.where(market_id: params[:market_id])
-		render :index
 	end
+
+	def new
+		@market = Market.new
+	end
+
+	def create
+		@market = Market.create(market_create_params[:market])
+		redirect_to markets_path
+	end
+
+	def edit
+		@market = Market.find(params[:id])
+	end
+
+	def update
+		@market = Market.find(params[:id])
+		@market.update_attributes(market_update_params[:market])
+		redirect_to markets_path
+	end
+
+	private
+
+	def market_create_params
+		params.permit(market: [:name, :street, :city, :county, :state, :zip])
+	end
+
+		def market_update_params
+		params.permit(market: [:name, :street, :city, :county, :state, :zip])
+	end
+
 	
 end
