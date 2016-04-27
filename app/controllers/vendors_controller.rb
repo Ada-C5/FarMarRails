1bc936ee779d294cc3cafe047c21b8cec05d9869
Nadine Curry <nlcurry@gmail.com>
2016-04-27 09:54:49 -0700

    Made vendor index and show page

--- app/controllers/vendors_controller.rb
+++ app/controllers/vendors_controller.rb
@@ -1,2 +1,14 @@
 class VendorsController < ApplicationController
+  def index
+    @vendors = Vendor.order(id: :asc)
+  end
+
+  def show
+    @vendor = Vendor.find(params[:id])
+  end
+
+  def products
+    @vendor = Vendor.find(params[:id])
+  end
+
 end
--- config/routes.rb
+++ config/routes.rb
@@ -5,6 +5,8 @@ Rails.application.routes.draw do
   resources :vendors
   resources :products
   resources :sales, except: [:destroy, :update, :edit]
+
+  get "/vendors/:id/products" => "vendor#products"
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
