d345bff5e4d2cc24101dd094bac95cb8adbc42ba
Lisa Rolczynski <lisa.sano@gmail.com>
2016-04-27 15:08:59 -0700

    created form data for all markets in a partial. render partial in new and edit pages. added methods for #new, #create, #edit, #update to the controller

--- app/controllers/markets_controller.rb
+++ app/controllers/markets_controller.rb
@@ -1,2 +1,32 @@
 class MarketsController < ApplicationController
+
+  def new
+    @market = Market.new
+    render :new
+  end
+
+  def create
+    @market = Market.new(markets_params[:market])
+    if @market.save
+      redirect_to markets_path
+    else
+      render :new
+    end
+  end
+
+  def edit
+    @market = Market.find(params[:id])
+    render :edit
+  end
+
+  def update
+    Market.update(params[:id], markets_params[:market])
+    redirect_to markets_path
+  end
+end
+
+private
+
+def markets_params
+  params.permit(market: [:name, :address, :city, :county, :state, :zip])
 end
