1c6278fefdda57c4d4b6646c5a136555345385fc
Valerie <val.conklin@gmail.com>
2016-04-27 15:09:11 -0700

    Made it so show_vendor view now shows a list of all the products that vendor sells

--- app/controllers/vendors_controller.rb
+++ app/controllers/vendors_controller.rb
@@ -1,7 +1,13 @@
 class VendorsController < ApplicationController
   def index
-    @vendors = Vendor.all
+    @all_vendors = Vendor.all
     render :index
+  end
+
+  def show
+    @vendor = Vendor.find(params[:id])
+    @all_products = Product.all
+    render :show_vendor
   end
 
 end
