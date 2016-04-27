b297e1add61be48ffc26cc7401fa5430d06a322f
Risha <rishallen@gmail.com>
2016-04-27 15:28:45 -0700

    Added to the User functionality: - a list of all markets once the user clicks onto the market link:   - adding a method to the index in the Market controller - a view of the specific market:   - adding a show method in the Market controller - added the root_path link for the user to return back to the home page

--- app/controllers/markets_controller.rb
+++ app/controllers/markets_controller.rb
@@ -1,2 +1,14 @@
 class MarketsController < ApplicationController
+
+  def index
+    @markets = Market.order(id: :asc)
+  end
+
+  def show
+    # display the market info
+    # set to an instance method
+    # find is a class method so call on the class Market 
+    @markets = Market.find(params[:id])
+  end
+
 end
