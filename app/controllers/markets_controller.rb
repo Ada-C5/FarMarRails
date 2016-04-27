f1149f10501d5aa327f01f05310f69f986e97d3d
Suzanne Harrison <harrison_family@hotmail.com>
2016-04-27 12:06:58 -0700

    IN MARKETS added markets index method, and market show method to controller and added views for index and show.

---
+++ app/controllers/markets_controller.rb
@@ -0,0 +1,12 @@
+class MarketsController < ApplicationController
+  def index
+    @all_markets = Market.order(name: :asc)
+    render :index
+  end
+
+  def show
+    @market = Market.find(params[:id])
+    render :show
+end
+
+end
