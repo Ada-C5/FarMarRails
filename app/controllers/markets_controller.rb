38a002f069e5d4b93403aa09da421c45df70c6e3
Cristal Tay <bluecolorpencils@gmail.com>
2016-04-27 15:57:14 -0700

    there's ability to add a new market. edit seems to be broken.. shall fix it tomorrow.

--- app/controllers/markets_controller.rb
+++ app/controllers/markets_controller.rb
@@ -8,4 +8,34 @@ class MarketsController < ApplicationController
     @vendors = Vendor.where(market_id: params[:id])
   end
 
+  def new
+    @market = Market.new
+  end
+
+  def create
+    @market = Market.new(market_edit_params[:market])
+    if @market.save
+      redirect_to root_path
+    else
+      render :new
+    end
+  end
+
+  def edit
+    @market = Market.find(params[:id])
+  end
+
+  def update
+    @market = Market.update(params[:id],market_edit_params[:market])
+    if @market.save
+      redirect_to root_path
+    else
+      render :edit
+    end
+  end
+
+  private
+  def market_edit_params
+    params.permit!
+  end
 end
