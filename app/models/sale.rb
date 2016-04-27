1b0e9afd606bdd123acf19fa9c5bc4014274cf92
Yordanos <yordanosd@gmail.com>
2016-04-27 15:30:11 -0700

    added validation to our models and ran seed

--- app/models/sale.rb
+++ app/models/sale.rb
@@ -1,4 +1,6 @@
 class Sale < ActiveRecord::Base
+	validates :amount, :purchase_time, :vendor_id, :product_id, presence: true
+
 	belongs_to :vendor
 	belongs_to :product
 end
--- app/models/vendor.rb
+++ app/models/vendor.rb
@@ -1,4 +1,6 @@
 class Vendor < ActiveRecord::Base
+	validates :name, :market_id, presence: true
+
 	belongs_to :market
 	has_many :sales
 	has_many :products
