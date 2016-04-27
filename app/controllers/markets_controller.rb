be198a079d7523503aaa15844b4a852e3b348d7b
Suzanne Harrison <harrison_family@hotmail.com>
2016-04-27 15:50:25 -0700

    MARKET added edit view and routes, as well as methods in controller.

--- app/controllers/markets_controller.rb
+++ app/controllers/markets_controller.rb
@@ -15,4 +15,39 @@ class MarketsController < ApplicationController
     @all_vendors = Vendor.all
   end
 
+  def create
+    @market = Market.new(market_create_params[:market])
+    if(@market.save)
+      redirect_to market_path(@market.id)#redirect in case user tries to post another form - brings them to entered view
+    else
+      render :new
+    end
+  end
+
+  def edit
+    @market = Market.find(params[:id])
+    @all_vendors = Vendor.all
+    render :edit
+  end
+
+  def update
+    @market = Market.find(params[:id])
+    if @market.update
+      redirect_to market_path(@market.id)#redirect in case user tries to post another form - brings them to entered view
+    else
+      render :edit
+    end#redirect in case user tries to post another form - brings them to entered view
+end
+
+  private
+
+    def market_create_params#the params we want when we create a task, to sanitize and control user input
+      params.permit(market: [:name, :address, :city, :county, :state, :zip_code])#these are the only parameters that can be passed from user.
+    end
+
+    def market_edit_params
+      params.permit(market: [:name, :address, :city, :county, :state, :zip_code])
+    end
+
+
 end
