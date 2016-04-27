060ebeb63f2062d250ccef1248ab2f502f49fb88
Sarah Nahmias <nahmisa@gmail.com>
2016-04-27 09:30:52 -0700

    Set up all actions in MarketContoller

--- app/controllers/markets_controller.rb
+++ app/controllers/markets_controller.rb
@@ -7,7 +7,56 @@ class MarketsController < ApplicationController
 
   def show
     @market = Market.find(params[:id])
-    
+
+  end
+
+  def new
+    # give them a shell and invite them to fill out the data.  Allows introspection into the object in the view!
+    @market = Market.new
+  end
+
+  def create
+    @market = Market.new(market_create_params[:task])
+
+    #any validation?
+    if @market.save
+      redirect_to root_path
+    else
+      render :new
+    end
+
+  end
+
+  def edit
+    @market = Market.find(params[:id])
+  end
+
+  def update
+    @market = Market.update(params[:id], market_edit_params[:task])
+
+    if @market.save
+      redirect_to root_path
+    else
+      render :edit
+    end
+
+  end
+
+  # delete a task
+  def destroy
+    @market = Market.delete(params[:id]) # need a private method?
+    redirect_to root_path
+  end
+
+  private
+
+  def market_create_params
+    params.permit(market: [:name, :address, :city, :county, :state, :zip])
+  end
+
+
+  def market_edit_params
+    params.permit(market: [:name, :address, :city, :county, :state, :zip, :updated_at])
   end
 
 end
