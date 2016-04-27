c18088ca053caf4fdab1a51efd5c1ea3620d9726
Anna Wilson <annie.marie.mason@gmail.com>
2016-04-27 14:14:38 -0700

    Updated db/seeds.rb. It will seed Market, Product, Sale and Vendor.

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,7 +1,70 @@
-# This file should contain all the record creation needed to seed the database with its default values.
-# The data can then be loaded with the rake db:seed (or created alongside the db with db:setup).
-#
-# Examples:
-#
-#   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
-#   Mayor.create(name: 'Emanuel', city: cities.first)
+# rake db:seed
+
+require 'csv'
+
+###VENDORS SEEDING###
+
+csv_text = File.read(Rails.root.join('lib', 'seeds', 'vendors.csv'))
+
+csv = CSV.parse(csv_text, :headers => true)
+
+csv.each do |row|
+  t = Vendor.new
+    t.name = row["name"]
+    t.employee_num = row["employee_num"]
+    t.market_id = row["market_id"]
+  t.save
+end
+
+puts "There are now #{Vendor.count} rows in the Vendor table"
+
+###PRODUCTS SEEDING###
+
+csv_text = File.read(Rails.root.join('lib', 'seeds', 'products.csv'))
+
+csv = CSV.parse(csv_text, :headers => true)
+
+csv.each do |row|
+  t = Product.new
+    t.name = row["name"]
+    t.vendor_id = row["vendor_id"]
+  t.save
+end
+
+puts "There are now #{Product.count} rows in the Product table"
+
+###SALES SEEDING###
+
+csv_text = File.read(Rails.root.join('lib', 'seeds', 'sales.csv'))
+
+csv = CSV.parse(csv_text, :headers => true)
+
+csv.each do |row|
+  t = Sale.new
+    t.amount = row["amount"]
+    t.purchase_time = row["purchase_time"]
+    t.vendor_id = row["vendor_id"]
+    t.product_id = row["product_id"]
+  t.save
+end
+
+puts "There are now #{Sale.count} rows in the Sale table"
+
+###MARKETS SEEDING###
+
+csv_text = File.read(Rails.root.join('lib', 'seeds', 'markets.csv'))
+
+csv = CSV.parse(csv_text, :headers => true)
+
+csv.each do |row|
+  t = Market.new
+    t.name = row["name"]
+    t.address = row["address"]
+    t.city = row["city"]
+    t.county = row["county"]
+    t.state = row["state"]
+    t.zip = row["zip"]
+  t.save
+end
+
+puts "There are now #{Market.count} rows in the Market table"
