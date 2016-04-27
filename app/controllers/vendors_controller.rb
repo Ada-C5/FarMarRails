d2f19d1a9b4ce1bd08ec39185ec7c0acacda7c3b
Jillian Boshart <Jilliliz@gmail.com>
2016-04-27 09:35:44 -0700

    Started work on Vendors controller methods.

--- app/controllers/vendors_controller.rb
+++ app/controllers/vendors_controller.rb
@@ -1,2 +1,32 @@
 class VendorsController < ApplicationController
+
+  def index
+    @vendors = Vendor.find.all
+    render :index
+  end
+
+  def show
+    @vendors_listed = Vendor.find.(params[:id])
+    render :index
+  end
+
+  def new
+    @new_vendor = Vendor.new
+  end
+
+  def create
+
+  end
+
+  def edit
+
+  end
+
+  def update
+
+  end
+
+  def destroy
+
+  end
 end
