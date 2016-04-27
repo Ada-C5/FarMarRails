fe0d1f8e5547b6da685d73204029324946f9d2b3
justine <justine.winnie@gmail.com>
2016-04-27 09:35:02 -0700

    created routes.

--- config/routes.rb
+++ config/routes.rb
@@ -53,4 +53,43 @@ Rails.application.routes.draw do
   #     # (app/controllers/admin/products_controller.rb)
   #     resources :products
   #   end
+
+
+#   Prefix Verb   URI Pattern                  Controller#Action
+#         root GET    /                            markets#index
+#      markets GET    /markets(.:format)           markets#index
+#              POST   /markets(.:format)           markets#create
+#   new_market GET    /markets/new(.:format)       markets#new
+#  edit_market GET    /markets/:id/edit(.:format)  markets#edit
+#       market GET    /markets/:id(.:format)       markets#show
+#              PATCH  /markets/:id(.:format)       markets#update
+#              PUT    /markets/:id(.:format)       markets#update
+#      vendors GET    /vendors(.:format)           vendors#index
+#              POST   /vendors(.:format)           vendors#create
+#   new_vendor GET    /vendors/new(.:format)       vendors#new
+#  edit_vendor GET    /vendors/:id/edit(.:format)  vendors#edit
+#       vendor GET    /vendors/:id(.:format)       vendors#show
+#              PATCH  /vendors/:id(.:format)       vendors#update
+#              PUT    /vendors/:id(.:format)       vendors#update
+#              DELETE /vendors/:id(.:format)       vendors#destroy
+#        sales GET    /sales(.:format)             sales#index
+#              POST   /sales(.:format)             sales#create
+#     new_sale GET    /sales/new(.:format)         sales#new
+#         sale GET    /sales/:id(.:format)         sales#show
+#     products GET    /products(.:format)          products#index
+#              POST   /products(.:format)          products#create
+#  new_product GET    /products/new(.:format)      products#new
+# edit_product GET    /products/:id/edit(.:format) products#edit
+#      product GET    /products/:id(.:format)      products#show
+#              PATCH  /products/:id(.:format)      products#update
+#              PUT    /products/:id(.:format)      products#update
+#              DELETE /products/:id(.:format)      products#destroy
+
+
+  root 'markets#index'
+  resources :markets, except:[:destroy]
+  resources :vendors
+  resources :sales, except:[:edit, :destroy, :update]
+  resources :products
+  
 end
