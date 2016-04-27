262283ef9c4df20ca860af95cbbaa2c85661d6d4
Suzanne Harrison <harrison_family@hotmail.com>
2016-04-27 15:13:40 -0700

    MARKET added form and view to add a new market

--- app/controllers/markets_controller.rb
+++ app/controllers/markets_controller.rb
@@ -6,8 +6,13 @@ class MarketsController < ApplicationController
 
   def show
     @market = Market.find(params[:id])
-    @all_vendors = Vendor.all 
+    @all_vendors = Vendor.all
     render :show
+  end
+
+  def new
+    @market = Market.new
+    @all_vendors = Vendor.all
   end
 
 end
