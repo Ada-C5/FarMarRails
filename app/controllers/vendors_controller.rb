148ecfa9479115f49536ad7a125e799915fc848d
Anna Wilson <annie.marie.mason@gmail.com>
2016-04-28 14:10:42 -0700

    Market can delete a vendor

--- app/controllers/vendors_controller.rb
+++ app/controllers/vendors_controller.rb
@@ -4,11 +4,44 @@ class VendorsController < ApplicationController
       @vendors = Vendor.all
     else
       @vendors = Vendor.where(market_id: params[:market_id])
+      @market_edit = true
     end
   end
 
   def show
     @vendor = Vendor.find(params[:id])
   end
-  
+
+  def new
+    @vendor = Vendor.new
+  end
+
+  def create
+    @vendor = Vendor.new(vendor_create_params[:vendor])
+    if(@vendor.save)
+      redirect_to vendors_path
+    else
+      render :add
+    end
+  end
+
+  def edit
+    @vendor = Vendor.find(params[:id])
+  end
+
+  def update
+    vendor = Vendor.find(params[:id])
+    vendor.update!(vendor_create_params[:vendor])
+    redirect_to root_path
+  end
+
+  def destroy
+    vendor = Vendor.find(params[:id]).destroy
+    redirect_to market_vendors_path(params[:market_id])
+  end
+
+  def vendor_create_params
+    params.permit(vendor: [:name, :employee_num, :market_id])
+  end
+
 end
