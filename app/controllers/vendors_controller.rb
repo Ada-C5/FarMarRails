278029a2b64e3cfe997eb62e607264a675371170
Jade Vance <jadenicolevance@gmail.com>
2016-04-28 15:06:02 -0700

    Added in new/create for vendors controller. Created a partial for vendor creation form.

--- app/controllers/vendors_controller.rb
+++ app/controllers/vendors_controller.rb
@@ -8,6 +8,25 @@ class VendorsController < ApplicationController
     @vendor ||= Vendor.find(params[:vendor_id])
   end
 
+  def new 
+    @vendor = Vendor.new
+    @market = Market.find(params[:market_id])
+  end
 
+  def create
+    @vendor = Vendor.new(new_params[:vendor])
+    if @vendor.save
+      @vendor.update(market_id: params[:market_id])
+      redirect_to market_path(params[:market_id])
+    else
+      render :new
+    end 
+  end
+
+  private
+
+  def new_params
+    params.permit(vendor: [:name, :number_of_employees])
+  end
 
 end
