05dc9c780323538651cc304d2ed207a2b7134176
sophia <sophiabaldonado@gmail.com>
2016-04-28 16:35:20 -0700

    Vendors can create sales, they can only sell their products, the purchase_time is determined by time that the vendor submits the sale;

--- app/controllers/sales_controller.rb
+++ app/controllers/sales_controller.rb
@@ -7,8 +7,13 @@ class SalesController < ApplicationController
   def create
     @vendor = Vendor.find(params[:vendor_id])
     @sale = Sale.new(create_sale_params[:sale])
+    # raise
     if @sale.save
-      @sale.update(vendor_id: params[:vendor_id])
+      # We thought the purchase time should be generated at the time the Sale is created
+      # as if from a POS system, we didn't think Vendor should have to manually input
+      # the date and time of the sale
+      @sale.update(vendor_id: params[:vendor_id], purchase_time: Time.now, product_id: create_sale_params[:sale][:product_id])
+      # raise
       redirect_to vendor_path(params[:vendor_id])
     else
       render :new
@@ -17,7 +22,7 @@ class SalesController < ApplicationController
 
   private
   def create_sale_params
-    params.permit(sale: [:amount, :purchase_time])
+    params.permit(sale: [:amount, :product_id])
   end
 
 end
