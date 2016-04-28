ecd7f2518a4cfb332d384757c520011794d2e0e4
justine <justine.winnie@gmail.com>
2016-04-28 15:47:42 -0700

    added delete button to vendors on market page.

--- app/controllers/vendors_controller.rb
+++ app/controllers/vendors_controller.rb
@@ -8,4 +8,13 @@ class VendorsController < ApplicationController
     @vendor = Vendor.find(params[:id])
   end
 
+  def destroy
+    @vendor = Vendor.find(params[:id])
+    if @vendor.destroy
+      redirect_to market_path(@vendor.market_id)
+    else
+      flash.now[:alert] = 'Vendor could not be deleted.'
+    end
+  end
+
 end
