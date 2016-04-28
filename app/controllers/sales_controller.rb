4dc4286ee3b7de1a675e8b25964a997ed6c08d60
Jillian Boshart <Jilliliz@gmail.com>
2016-04-28 14:37:03 -0700

    After much consternation, the create sale method works.

--- app/controllers/sales_controller.rb
+++ app/controllers/sales_controller.rb
@@ -13,40 +13,22 @@ class SalesController < ApplicationController
   end
 
   def create
-    @sale = Sale.new(new_product_create_params[:sale])
+#I HATE THIS IT IS THE WORST:
+    params[:sale][:amount] = params[:sale][:amount].to_s.gsub(/\./, '')
+#END OF HATE
+    @sale = Sale.new(new_sale_create_params[:sale])
     if @sale.save
       @sale[:sale_id] = @sale[:id]
-      @sale[:product_id] = @product[:id]
-      @sale[:vendor_id] = @vendor[:id]
       @sale.save
-      redirect_to product_path(@product.vendor_id)
+      redirect_to product_path(@sale.vendor_id)
     else
       render :new
     end
   end
 
-  # def edit
-  #   @product = Product.find(params[:id])
-  #   render :edit
-  # end
-  #
-  # def update
-  #   @product = Product.find(params[:id])
-  #   if @product.update(new_product_create_params[:product])
-  #     redirect_to product_path(@product.vendor_id)
-  #   else
-  #     render :edit
-  #   end
-  # end
-  #
-  # def destroy
-  #   @product = Product.destroy(params[:id])
-  #   redirect_to product_path(@product.vendor_id)
-  # end
-
   private
 
   def new_sale_create_params
-    params.permit(sale: [:sale_id, :amount, :vendor_id, :product_id]) #you must permit parameters when you want to allow access to the user to create new data using the params. The user is ONLY permitted to access the artist and title parameters.
+    params.permit(sale: [:sale_id, :amount, :vendor_id, :product_id])
   end
 end
