c6111db4ee9ad414d13e23a1da7c1dd2bc1ccde7
Adriana <dri19tcc@gmail.com>
2016-04-27 21:34:46 -0700

    On Vendors show also listed the products associated with that vendor.  Added a delete method to the products

--- app/controllers/products_controller.rb
+++ app/controllers/products_controller.rb
@@ -1,2 +1,23 @@
 class ProductsController < ApplicationController
+  def edit
+
+  end
+
+  def update
+
+  end
+
+  def new
+
+  end
+
+  def create
+
+  end
+
+  def destroy
+    product = Product.find(params[:id]).destroy
+
+    redirect_to vendor_path(params[:vendor_id])
+  end
 end
