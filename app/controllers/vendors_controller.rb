3bd5cadbcc85b3d3d98dcc20a83391126f20f88d
Jessica <jessica.weeber@gmail.com>
2016-04-28 14:29:44 -0700

    created edit and delete for vendors. working on default for dropdown when creating a new vendor

--- app/controllers/vendors_controller.rb
+++ app/controllers/vendors_controller.rb
@@ -9,13 +9,18 @@ class VendorsController < ApplicationController
   end
 
   def new
-    @vendor = Vendor.new  #creates vendor so that we can access the form
+    # @market = Market.find(@vendor.market_id)
+    # @vendor = Vendor.find(params[:id])
+    @vendor = Vendor.new  #creates vendor so that we can access the form 
+    if @vendor.market_id
+      @current_market = @vendor.market.id
+    end
   end
 
   def create
-    @vendor = Vendor.new(vendor_create_params[:vendor])
+    @vendor = Vendor.create(vendor_create_params[:vendor])
     if @vendor.save
-      redirect_to :index
+      redirect_to markets_path
     else
       render :new
     end
@@ -23,23 +28,33 @@ class VendorsController < ApplicationController
 
   def edit
     @vendor = Vendor.find(params[:id])
+    if @vendor.id 
+      @current_market = @vendor.market.id
+    end
     render :new
   end
 
   def update
     @vendor = Vendor.find(params[:id])
-    @vendor.update(vendor_update_params[:vendor])
-    redirect_to root_path
+    @vendor.update_attributes(vendor_update_params[:vendor])
+    redirect_to markets_path
+  end
+
+  def destroy
+    @vendors = Vendor.find(params[:id])
+    @market = Market.find(@vendors.market_id)
+    @vendors.destroy
+    redirect_to market_path(@market)
   end
 
   private
 
   def vendor_create_params
-    params.permit(vendor: [:name, :employee_count])
+    params.permit(vendor: [:name, :employee_count, :market_id])
   end
 
   def vendor_update_params
-    params.permit(vendor: [:name, :employee_count])
+    params.permit(vendor: [:name, :employee_count, :market_id])
   end
 
 end
