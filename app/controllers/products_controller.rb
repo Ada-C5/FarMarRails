5947724d0ddedc82520f47c7811932202b0a10a9
Valerie <val.conklin@gmail.com>
2016-04-28 13:39:38 -0700

    Added some gems and started working on create method for products

--- app/controllers/products_controller.rb
+++ app/controllers/products_controller.rb
@@ -1,7 +1,19 @@
 class ProductsController < ApplicationController
 
   def new
+    @product = Product.new
+    @all_vendors = Vendor.all
     render :new
   end
 
+  def create
+    @product = Product.create(product_create_params[:product])
+    redirect_to vendor_path(@product.vendor_id)
+  end
+
+  private
+
+  def product_create_params
+    params.permit(product: [:name, :vendor_id])
+  end
 end
