0d116e472a00b15a8d1ecaa6baf39ff32b809c52
Lisa Rolczynski <lisa.sano@gmail.com>
2016-04-28 15:31:20 -0700

    shows all sales for the current month in the vendors/:id/sales path. if there are no sales, tells the user that there are no sales this month

--- app/controllers/sales_controller.rb
+++ app/controllers/sales_controller.rb
@@ -3,5 +3,6 @@ class SalesController < ApplicationController
     @vendor = Vendor.find(params[:id])
     @sales = @vendor.sales
     @sum = Sale.where(vendor_id: params[:id]).sum(:amount)
+    @sales_this_month = Sale.sales_this_month(Time.now.strftime("%m"), params[:id])
   end
 end
--- app/models/sale.rb
+++ app/models/sale.rb
@@ -1,4 +1,9 @@
 class Sale < ActiveRecord::Base
   belongs_to :product
   belongs_to :vendor
+
+  def self.sales_this_month(desired_month, ven_id)
+    where("strftime('%m', purchase_time) = ?", desired_month).where(vendor_id: ven_id)
+  end
 end
+ 
