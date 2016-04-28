b5f054c9307a06a909174d2b91b48f88b8c25cb7
Sarah Nahmias <nahmisa@gmail.com>
2016-04-28 15:10:17 -0700

    Moved products form to partial and added validation and errors messages to products

--- app/models/product.rb
+++ app/models/product.rb
@@ -1,4 +1,8 @@
 class Product < ActiveRecord::Base
   belongs_to :vendor
   has_many :sales
+
+  # Product must have a name, but the name may not necessarily be unique
+  validates :name, presence: true
+
 end
