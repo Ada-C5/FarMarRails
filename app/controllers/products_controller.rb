4fa24ad1ab8b5f3f342e5a5907dda4895166f186
Adriana <dri19tcc@gmail.com>
2016-04-28 15:10:51 -0700

    Added the edit function to work.  Updated the show for what is displayed on the screen.

--- app/controllers/products_controller.rb
+++ app/controllers/products_controller.rb
@@ -1,10 +1,13 @@
 class ProductsController < ApplicationController
   def edit
-
+    @product = Product.find(params[:id])
   end
 
   def update
+    @product = Product.find(params[:id])
+    @product.update(product_update_params[:product])
 
+    redirect_to edit_vendor_product_path
   end
 
   def new
@@ -19,5 +22,11 @@ class ProductsController < ApplicationController
     product = Product.find(params[:id]).destroy
 
     redirect_to vendor_path(params[:vendor_id])
+  end
+
+  private
+
+  def product_update_params
+    params.permit(product: [:name])
   end
 end
