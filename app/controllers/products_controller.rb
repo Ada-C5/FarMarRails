b9acbb4ab1b69c703a796a20da2269f4f11034da
Valerie <val.conklin@gmail.com>
2016-04-28 14:24:37 -0700

    Got Add Product functionality working yaaaay nested resources are hard yaaaay

--- app/controllers/products_controller.rb
+++ app/controllers/products_controller.rb
@@ -2,18 +2,22 @@ class ProductsController < ApplicationController
 
   def new
     @product = Product.new
-    @all_vendors = Vendor.all
     render :new
   end
 
   def create
-    @product = Product.create(product_create_params[:product])
-    redirect_to vendor_path(@product.vendor_id)
+    @product = Product.new(product_create_params[:product])
+    @product.vendor_id = params[:vendor_id]
+    if(@product.save)
+      redirect_to vendor_path(params[:vendor_id])
+    else
+      render :new
+    end
   end
 
   private
 
   def product_create_params
-    params.permit(product: [:name, :vendor_id])
+    params.permit(product: [:name])
   end
 end
