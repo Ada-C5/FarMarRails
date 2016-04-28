c9bf8c99756a74d5a10bd5b8e000fa0a2f7f92ef
Lisa Rolczynski <lisa.sano@gmail.com>
2016-04-28 13:16:47 -0700

    show the total sale amount for a particular vendor on the sales index page. updated view of sale list info to be in table format.

--- app/controllers/sales_controller.rb
+++ app/controllers/sales_controller.rb
@@ -2,5 +2,6 @@ class SalesController < ApplicationController
   def index
     @vendor = Vendor.find(params[:id])
     @sales = @vendor.sales
+    @sum = Sale.where(vendor_id: params[:id]).sum(:amount)
   end
 end
