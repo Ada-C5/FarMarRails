810c48fe56925a9dd76a6d7740fdb6003c9b6747
justine <justine.winnie@gmail.com>
2016-04-28 16:45:04 -0700

    added button to edit product

--- app/controllers/products_controller.rb
+++ app/controllers/products_controller.rb
@@ -13,4 +13,29 @@ class ProductsController < ApplicationController
     end
   end
 
+  def edit
+  end
+
+  def update
+  end
+
+  def edit
+    @product = Product.find(params[:id])
+  end
+
+  def update
+    @product = Product.find(params[:id])
+    if @product.update(product_edit_params[:product])
+      redirect_to vendor_path(@product.vendor_id)
+    else
+      render :edit
+    end
+  end
+
+  private
+
+  def product_edit_params
+    params.permit(product: [:name])
+  end
+
 end
