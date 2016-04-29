6bb3f4fdb7b502692bcec0fb746e0ecf3c93eaaf
Ania Gonzalez <aniagr@gmail.com>
2016-04-29 09:23:45 -0700

    Added markets controller and index, new, edit and update actions and views.

--- config/routes.rb
+++ config/routes.rb
@@ -54,7 +54,7 @@ Rails.application.routes.draw do
   #     resources :products
   #   end
 
-resources :markets, only: [:index, :show]
+resources :markets
 resources :vendors, only: [:index, :show]
 
 root 'home#index'
