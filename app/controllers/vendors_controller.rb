4a0552c24ae42736e61946a3f0da1d228c1ba2ef
Ania Gonzalez <aniagr@gmail.com>
2016-04-29 10:54:16 -0700

    Added functionality so that Markets can Add, Delete and Edit Vendors

--- app/controllers/vendors_controller.rb
+++ app/controllers/vendors_controller.rb
@@ -7,6 +7,45 @@ class VendorsController < ApplicationController
    @vendors_id = Vendor.find(params[:id])
   end
 
+  def new
+    @vendor = Vendor.new
+  end
 
+  def create
+    @vendor = Vendor.new(vendor_params[:vendor])
+    if @vendor.save
+      redirect_to market_path(id: @vendor.market_id)
+    end
+  end
+
+  def edit
+    @vendor = Vendor.find(params[:id])
+    render :new
+  end
+
+  def update
+    @vendor = Vendor.find(params[:id])
+    if @vendor.update(vendor_update_params[:vendor])
+      redirect_to market_path(id: @vendor.market_id)
+    else
+      render :new
+    end
+  end
+
+  def destroy
+    @vendor = Vendor.find(params[:id])
+    @vendor.destroy
+    redirect_to market_path(id: @vendor.market_id)
+  end
+
+private
+
+def vendor_params
+  params.permit(vendor: [:name, :no_of_employees, :market_id])
+end
+
+def vendor_update_params
+  params.permit(vendor: [:name, :no_of_employees, :market_id])
+end
 
 end
