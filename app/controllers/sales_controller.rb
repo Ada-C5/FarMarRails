a283a1c501e774963397fc3fe952776c2adf0b70
Sarah <sarahkane682@yahoo.com>
2016-04-29 14:35:02 -0700

    add new sale for product

--- app/controllers/sales_controller.rb
+++ app/controllers/sales_controller.rb
@@ -1,15 +1,34 @@
 class SalesController < ApplicationController
 
-def index
-  @sales = Sale.where(vendor_id: params[:vendor_id])
-  render :index
-end
+  def index
+    @sales = Sale.where(vendor_id: params[:vendor_id])
+    render :index
+  end
 
 
-def show
-  @sales = Sale.all.where(params[:vendor_id])
-end
+  def show
+    @sales = Sale.all.where(params[:vendor_id])
+  end
+
+  def new
+    @vendor = Vendor.find(params[:vendor_id])
+    @sale = @vendor.sales.new
+  end
+
+  def create
+    @vendor = Vendor.find(params[:vendor_id])
+    @sale = @vendor.sales.new(sales_params)
+      if  @sale.save
+      redirect_to vendor_sales_path
+      end
+
+  end
 
 
+    private
+
+    def sales_params
+      params.require(:sale).permit(:amount, :purchase_time)
+    end
 
 end
