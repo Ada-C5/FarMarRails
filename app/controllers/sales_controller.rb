2dbe1155a746ac227cddbe11410fe6558a4e1b7c
Sarah <sarahkane682@yahoo.com>
2016-04-29 11:33:11 -0700

    added sales

---
+++ app/controllers/sales_controller.rb
@@ -0,0 +1,13 @@
+class SalesController < ApplicationController
+
+def index
+  @sales = Sale.where(vendor_id: params[:vendor_id])
+  render :index
+end
+
+
+def show
+  @sales = Sale.all.where(params[:vendor_id])
+end
+
+end
